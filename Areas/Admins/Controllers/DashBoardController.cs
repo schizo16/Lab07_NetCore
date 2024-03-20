bae86aea502a4d515ae552ebcdbed6ac7d906b35
Batman <Batman@DESKTOP-6V9DUQE>
2024-03-20 18:32:05 +0700

    Add project files.

---
+++ Areas/Admins/Controllers/DashBoardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab07_NetCore.Areas.Admins.Controllers
+{
+    [Area("Admin")]
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
